d69b78e4c65688fccba69409f0e52fa5865cec50
v.kogut <v.kogutmassmediagroup@gmail.com>
2017-12-22 16:00:42 +0200

    some

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './services/auth.service';
-import { RegisterComponent } from './components/register/register.component';
 import { StatusComponent } from './components/status/status.component';
 import { EnsureAuthenticated } from './services/ensure-authenticated.service';
 import { LoginRedirect } from './services/login-redirect.service';
@@ -17,7 +16,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent,
     StatusComponent,
     DashboardComponent,
   ],
@@ -29,11 +27,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
       {
         path: 'login',
         component: LoginComponent,
-        canActivate: [LoginRedirect]
-      },
-      {
-        path: 'register',
-        component: RegisterComponent,
         canActivate: [LoginRedirect]
       },
       {
